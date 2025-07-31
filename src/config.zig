c2c8831cd6ebc05b1d8834926a3f552d5c2cbf12
Júnior Nascimento <krymancer@protonmail.com>
2025-07-31 10:53:26 -0300

    getting config from env, ai is still bad

--- src/config.zig
+++ src/config.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const dotenv = @import("dotenv");
 
+const ConfigKeys = enum { PAYMENT_PROCESSOR_DEFAULT, PAYMENT_PROCESSOR_FALLBACK, ADDRESS, PORT };
+
 pub const Config = struct {
     port: u16,
     address: []const u8,
@@ -9,20 +11,29 @@ pub const Config = struct {
 
     pub fn init(allocator: std.mem.Allocator) !@This() {
         var envs = try dotenv.getDataFrom(allocator, ".env");
-        var it = envs.iterator();
+        defer envs.deinit();
 
-        while (it.next()) |*entry| {
-            std.debug.print(
-                "{s}={s}\n",
-                .{ entry.key_ptr.*, entry.value_ptr.*.? },
-            );
+        var config: @This() = undefined;
+
+        const enum_info = @typeInfo(ConfigKeys).@"enum";
+        inline for (enum_info.fields) |field| {
+            const enum_tag_name = field.name;
+
+            if (envs.contains(enum_tag_name)) {
+                const value = envs.get(enum_tag_name).?;
+                const key_enum = @field(ConfigKeys, enum_tag_name);
+
+                switch (key_enum) {
+                    .PAYMENT_PROCESSOR_DEFAULT => config.payment_processor_default_url = value.?,
+                    .PAYMENT_PROCESSOR_FALLBACK => config.payment_processor_fallback_url = value.?,
+                    .ADDRESS => config.address = value.?,
+                    .PORT => {
+                        config.port = try std.fmt.parseInt(u16, value.?, 10);
+                    },
+                }
+            }
         }
 
-        return .{
-            .port = 8080,
-            .address = "0.0.0.0",
-            .payment_processor_default_url = "http://payment-processor-default:8080/payments",
-            .payment_processor_fallback_url = "http://payment-processor-fallback:8080/payments",
-        };
+        return config;
     }
 };
